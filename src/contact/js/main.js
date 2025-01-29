d860459066a2a5033832445f6330d9d1b65a5c68
Jad El karchi <elkarchie2@gmail.com>
2025-01-29 21:46:28 +0100

    added contact us front-end page and form, responsiveness is good, need to be centered for small screens

--- src/contact/js/main.js
+++ src/contact/js/main.js
@@ -4,8 +4,3 @@ import { animateDomsWithObserver } from "../../files/utils.js";
 
 
 animateDomsWithObserver(animatedJsonData);
-setTimeout(() => {
-    const circlesDoms = document.getElementsByClassName("shape-circle-style");
-    circlesDoms[0].classList.add("float");
-    circlesDoms[1].classList.add("float");
-}, 1500)
